6d0ba7b41ab5bbf8ad4fde5a5fa53cb87ee7c577
unknown <s.o4eretniy@gmail.com>
2019-02-18 01:08:36 +0200

    add Excel file

--- shd.py
+++ shd.py
@@ -6,3 +6,16 @@ for i in range(15):
 
 print(len(list))
 print(list)
+# Create a list with the mood to write in excel
+
+import pandas as pd
+import xlrd
+import matplotlib.pyplot as plt
+
+# open Excel file
+sh = pd.ExcelFile('templates/shedule.xlsx')
+print(sh.sheet_names)
+new_sh = sh.parse('shedule')
+print(new_sh.columns)
+print(new_sh['Days'])
+print(new_sh['Mood'])
